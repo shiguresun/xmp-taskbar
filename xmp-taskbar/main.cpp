61ccb6ff1e0debe20c03b08b7064b21ed46e158a
shiguresun <34537122+shiguresun@users.noreply.github.com>
2026-06-11 16:30:39 +0900

    Update main.cpp
    main.cppをwin10でも動くように修正(したらしい)

--- xmp-taskbar/main.cpp
+++ xmp-taskbar/main.cpp
@@ -1,9 +1,8 @@
 /*
- * Copyright (C) 2016 FIX94
- *
- * This software may be modified and distributed under the terms
- * of the MIT license.  See the LICENSE file for details.
+ * xmp-taskbar Win10 fixed version
+ * Based on FIX94 original code
  */
+
 #include <windows.h>
 #include <shobjidl.h>
 #include <cstdio>
@@ -15,6 +14,7 @@ static ITaskbarList3* m_pTaskBarlist = NULL;
 static UINT_PTR timer_ptr = NULL;
 static CRITICAL_SECTION section;
 static HWND xmpwin;
+static bool comInit = false;
 
 static void WINAPI DSP_About(HWND win);
 static void *WINAPI DSP_New(void);
@@ -25,7 +25,7 @@ static BOOL WINAPI DSP_SetConfig(void *inst, void *config, DWORD size);
 
 static XMPDSP dsp = {
     XMPDSP_FLAG_NODSP,
-    "Taskbar Progress",
+    "Taskbar Progress (Win10 Fixed)",
     DSP_About,
     DSP_New,
     DSP_Free,
@@ -37,14 +37,9 @@ static XMPDSP dsp = {
 
 static void WINAPI DSP_About(HWND win)
 {
-	char mBoxChar[256];
-	sprintf(mBoxChar, "Taskbar Progress v0.3 for XMPlay\n"
-		"Copyright (C) 2016 FIX94\n"
-		"Built: %s %s", __DATE__, __TIME__);
-	MessageBox(win,
-		mBoxChar,
-		"About Plugin",
-		MB_ICONINFORMATION);
+    char mBoxChar[256];
+    sprintf(mBoxChar, "Taskbar Progress (Win10 Fixed)\nBuilt: %s %s", __DATE__, __TIME__);
+    MessageBox(win, mBoxChar, "About Plugin", MB_ICONINFORMATION);
 }
 
 static const char *WINAPI DSP_GetDescription(void *inst)
@@ -54,77 +49,95 @@ static const char *WINAPI DSP_GetDescription(void *inst)
 
 VOID CALLBACK updateTaskbar(HWND hwnd, UINT uMsg, UINT_PTR idEvent, DWORD dwTime)
 {
-	//try to get things updated
-	if (TryEnterCriticalSection(&section))
-	{
-		//SendMessage seems to be reliable when it comes to subsongs
-		int cPosMs = SendMessage(xmpwin, WM_USER, 0, IPC_GETOUTPUTTIME);
-		int lTotal = SendMessage(xmpwin, WM_USER, 1, IPC_GETOUTPUTTIME) * 1000;
-		//actual isPlaying function does not return pause status so use SendMessage
-		int playStatus = SendMessage(xmpwin, WM_USER, 0, IPC_ISPLAYING);
-		//we might actually be stopped but still called so clear progress if needed
-		if (playStatus == 0 || lTotal < 0 || cPosMs < 0)
-			m_pTaskBarlist->SetProgressState(hwnd, TBPF_NOPROGRESS);
-		else
-		{
-			if (playStatus == 3)
-				m_pTaskBarlist->SetProgressState(hwnd, TBPF_PAUSED);
-			else //everything except 3 for us means that its playing
-				m_pTaskBarlist->SetProgressState(hwnd, TBPF_INDETERMINATE);
-			m_pTaskBarlist->SetProgressValue(hwnd, min(cPosMs, lTotal), lTotal);
-		}
-		LeaveCriticalSection(&section);
-	}
+    if (!m_pTaskBarlist)
+        return;
+
+    if (TryEnterCriticalSection(&section))
+    {
+        if (!IsWindow(xmpwin))
+        {
+            LeaveCriticalSection(&section);
+            return;
+        }
+
+        int cPosMs = SendMessage(xmpwin, WM_USER, 0, IPC_GETOUTPUTTIME);
+        int lTotal = SendMessage(xmpwin, WM_USER, 1, IPC_GETOUTPUTTIME) * 1000;
+        int playStatus = SendMessage(xmpwin, WM_USER, 0, IPC_ISPLAYING);
+
+        if (playStatus == 0 || lTotal <= 0 || cPosMs < 0)
+        {
+            m_pTaskBarlist->SetProgressState(hwnd, TBPF_NOPROGRESS);
+        }
+        else
+        {
+            // Win10 では NORMAL を使う
+            if (playStatus == 3)
+                m_pTaskBarlist->SetProgressState(hwnd, TBPF_PAUSED);
+            else
+                m_pTaskBarlist->SetProgressState(hwnd, TBPF_NORMAL);
+
+            m_pTaskBarlist->SetProgressValue(hwnd, min(cPosMs, lTotal), lTotal);
+        }
+
+        LeaveCriticalSection(&section);
+    }
 }
 
 static void *WINAPI DSP_New()
 {
-	//Get Main Window for Updates
-	xmpwin=xmpfmisc->GetWindow();
-	//Create critical section for taskbar updates
-	InitializeCriticalSection(&section);
-	//Create TaskbarList Instance
-	CoCreateInstance(
-		CLSID_TaskbarList, NULL, CLSCTX_ALL,
-		IID_ITaskbarList3, (void**)&m_pTaskBarlist);
-	//If everything went well set start value
-	if (m_pTaskBarlist)
-	{
-		EnterCriticalSection(&section);
-		m_pTaskBarlist->SetProgressState(xmpwin, TBPF_NOPROGRESS);
-		LeaveCriticalSection(&section);
-		//Hook up a timer to the XMPlay window
-		timer_ptr = SetTimer(xmpwin, NULL, 20, updateTaskbar);
-	}
+    // COM 初期化（Win10 では必須）
+    HRESULT hr = CoInitializeEx(NULL, COINIT_APARTMENTTHREADED);
+    if (SUCCEEDED(hr))
+        comInit = true;
+
+    xmpwin = xmpfmisc->GetWindow();
+    InitializeCriticalSection(&section);
+
+    CoCreateInstance(
+        CLSID_TaskbarList, NULL, CLSCTX_ALL,
+        IID_ITaskbarList3, (void**)&m_pTaskBarlist);
+
+    if (m_pTaskBarlist)
+    {
+        EnterCriticalSection(&section);
+        m_pTaskBarlist->SetProgressState(xmpwin, TBPF_NOPROGRESS);
+        LeaveCriticalSection(&section);
+
+        timer_ptr = SetTimer(xmpwin, NULL, 20, updateTaskbar);
+    }
 
     return (void*)1;
 }
 
 static void WINAPI DSP_Free(void *inst)
 {
-	//Kill timer first
-	if (timer_ptr)
-	{
-		KillTimer(xmpwin, timer_ptr);
-		timer_ptr = NULL;
-	}
-	//then clean up taskbar
-	if (m_pTaskBarlist)
-	{
-		EnterCriticalSection(&section);
-		m_pTaskBarlist->SetProgressState(xmpwin, TBPF_NOPROGRESS);
-		m_pTaskBarlist->Release();
-		m_pTaskBarlist = NULL;
-		LeaveCriticalSection(&section);
-	}
-	//then get rid of the critical section
-	DeleteCriticalSection(&section);
+    if (timer_ptr)
+    {
+        KillTimer(xmpwin, timer_ptr);
+        timer_ptr = NULL;
+    }
+
+    if (m_pTaskBarlist)
+    {
+        EnterCriticalSection(&section);
+        m_pTaskBarlist->SetProgressState(xmpwin, TBPF_NOPROGRESS);
+        m_pTaskTaskBarlist->Release();
+        m_pTaskBarlist = NULL;
+        LeaveCriticalSection(&section);
+    }
+
+    DeleteCriticalSection(&section);
+
+    if (comInit)
+    {
+        CoUninitialize();
+        comInit = false;
+    }
 }
 
-// we dont have any config so these are just empty
 static DWORD WINAPI DSP_GetConfig(void *inst, void *config)
 {
-	return 0;
+    return 0;
 }
 
 static BOOL WINAPI DSP_SetConfig(void *inst, void *config, DWORD size)
@@ -132,13 +145,12 @@ static BOOL WINAPI DSP_SetConfig(void *inst, void *config, DWORD size)
     return TRUE;
 }
 
-// get the plugin's XMPDSP interface
 XMPDSP *WINAPI XMPDSP_GetInterface2(DWORD face, InterfaceProc faceproc)
 {
-	if (face != XMPDSP_FACE) return NULL;
-	xmpfmisc = (XMPFUNC_MISC*)faceproc(XMPFUNC_MISC_FACE); // import "misc" functions
-	xmpfstatus = (XMPFUNC_STATUS*)faceproc(XMPFUNC_STATUS_FACE); // import "status" functions
-	return &dsp;
+    if (face != XMPDSP_FACE) return NULL;
+    xmpfmisc = (XMPFUNC_MISC*)faceproc(XMPFUNC_MISC_FACE);
+    xmpfstatus = (XMPFUNC_STATUS*)faceproc(XMPFUNC_STATUS_FACE);
+    return &dsp;
 }
 
 BOOL WINAPI DllMain(HINSTANCE hDLL, DWORD reason, LPVOID reserved)
