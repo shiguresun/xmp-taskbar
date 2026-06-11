547a0c6c264ed8c10840f66849758b750e309e0d
shiguresun <34537122+shiguresun@users.noreply.github.com>
2026-06-11 19:05:25 +0900

    Update main.cpp
    諦めも肝心

--- main.cpp
+++ main.cpp
@@ -39,8 +39,8 @@ static void WINAPI DSP_About(HWND win)
 {
 sprintf(mBoxChar, "Taskbar Progress v0.3.1 for XMPlay\n"
 		"Copyright (C) 2026 Grin\n"
-		"Built: %s %s", __DATE__, __TIME__"\n"
-        "original code by FIX94");
+	    "original code by FIX94\n"
+		"Built: %s %s", __DATE__, __TIME__);
 }
 
 static const char *WINAPI DSP_GetDescription(void *inst)
